a0a05f5b6d1b4c24e2d4d8474f41fb88bd92e4c8
dogpie <dankov.ns@phystech.edu>
2022-04-04 20:29:45 +0300

    console support, some description

--- map_graphics.py
+++ map_graphics.py
@@ -5,7 +5,7 @@ import sys
 
 
 class MapGraphics:
-    def __init__(self, w, h, x_cells, y_cells, window):
+    def __init__(self, w, h, x_cells, y_cells, window, is_console):
         self.width = w
         self.height = h
         self.x_cells = x_cells
@@ -25,20 +25,22 @@ class MapGraphics:
         self.passes = {}
 
         self.window = window
+        if not is_console:
+            self.stop_button = button.Button(self.window, (
+                constants.WIDTH / 2 + constants.BIG_BUTTON_WIDTH / 2 + constants.MARGIN_TOP / 4, constants.MARGIN_TOP / 4,
+                constants.BIG_BUTTON_WIDTH,
+                constants.BIG_BUTTON_HEIGHT), "stop", self.stop)
 
         self.is_stopped = False
-        self.stop_button = button.Button(self.window, (
-            constants.WIDTH / 2 + constants.BIG_BUTTON_WIDTH / 2 + constants.MARGIN_TOP / 4, constants.MARGIN_TOP / 4,
-            constants.BIG_BUTTON_WIDTH,
-            constants.BIG_BUTTON_HEIGHT), "stop", self.stop)
 
-    def gen_map(self, map, passes):
+    def draw_map(self, map, passes, is_console):
         self.field = map
         self.passes = passes
-        for y in range(3 * self.y_cells):
-            for x in range(3 * self.x_cells):
-                if self.field[y][x] == 1:
-                    pygame.draw.rect(self.window, (0, 155, 0), self.cells[y][x], 0)
+        if not is_console:
+            for y in range(3 * self.y_cells):
+                for x in range(3 * self.x_cells):
+                    if self.field[y][x] == 1:
+                        pygame.draw.rect(self.window, (0, 155, 0), self.cells[y][x], 0)
 
     def draw_cell(self, x1, y1, x2, y2):
         valid_coords_1 = self.cells[y1][x1].topleft
@@ -57,24 +59,50 @@ class MapGraphics:
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.stop_button.click(event)
 
-    def draw_path(self, path):
+    def draw_path(self, path, is_console):
         self.is_stopped = False
-        for cell in range(self.cell, len(path) - 1):
-            if self.is_stopped:
-                self.cell = cell
-                return 0
-            v = path[cell]
-            to = path[cell + 1]
-            if v[0] == to[0]:
-                start = min(v[1], to[1])
-                end = max(v[1], to[1])
-                for i in range(1 + 3 * start, 1 + 3 * end):
-                    self.draw_cell(i, 1 + 3 * v[0], i + 1, 1 + 3 * v[0])
-            elif v[1] == to[1]:
-                start = min(v[0], to[0])
-                end = max(v[0], to[0])
-                for i in range(1 + 3 * start, 1 + 3 * end):
-                    self.draw_cell(1 + 3 * v[1], i, 1 + 3 * v[1], i + 1)
+        if not is_console:
+            for cell in range(self.cell, len(path) - 1):
+                if self.is_stopped:
+                    self.cell = cell
+                    return 0
+                v = path[cell]
+                to = path[cell + 1]
+                if v[0] == to[0]:
+                    start = min(v[1], to[1])
+                    end = max(v[1], to[1])
+                    for i in range(1 + 3 * start, 1 + 3 * end):
+                        self.draw_cell(i, 1 + 3 * v[0], i + 1, 1 + 3 * v[0])
+                elif v[1] == to[1]:
+                    start = min(v[0], to[0])
+                    end = max(v[0], to[0])
+                    for i in range(1 + 3 * start, 1 + 3 * end):
+                        self.draw_cell(1 + 3 * v[1], i, 1 + 3 * v[1], i + 1)
+        else:
+            for cell in range(self.cell, len(path) - 1):
+                v = path[cell]
+                to = path[cell + 1]
+                if v[0] == to[0]:
+                    start = min(v[1], to[1])
+                    end = max(v[1], to[1])
+                    for i in range(1 + 3 * start, 1 + 3 * end + 1):
+                        self.field[1 + 3 * v[0]][i] = 2
+                elif v[1] == to[1]:
+                    start = min(v[0], to[0])
+                    end = max(v[0], to[0])
+                    for i in range(1 + 3 * start, 1 + 3 * end + 1):
+                        self.field[i][1 + 3 * v[1]] = 2
+            for y in range(3 * self.y_cells):
+                for x in range(3 * self.x_cells):
+                    if self.field[y][x] == 1:
+                        print('#', end='')
+                    elif self.field[y][x] == 0:
+                        print(' ', end='')
+                    elif self.field[y][x] == 2:
+                        print('.', end='')
+                print()
+
+
         self.cell = 0
 
     def clear_display(self):
